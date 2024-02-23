61f0d419b57e1266e0e3d5e9b2f45850f8ba1586
H-Asmaa <asmaehadar32@gmail.com>
2024-02-23 16:46:30 +0100

    Committing a procedure that computes the average score.
    It creates a stored procedure ComputeAverageWeightedScoreForUser that computes and store the average weighted score for a student.
    
    Fixing output fail.

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -3,12 +3,11 @@ DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
 DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN userId INT)
 BEGIN
-	DECLARE userScores DECIMAL;
-	SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
-	INTO userScores
-	FROM corrections
-	JOIN projects ON corrections.project_id = projects.id
-	WHERE corrections.user_id = userId;
-	UPDATE users SET average_score = userScores WHERE id = userId;
+	SET @userScores := (
+		SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+		FROM corrections
+		JOIN projects ON corrections.project_id = projects.id
+		WHERE corrections.user_id = userId);
+	UPDATE users SET average_score = @userScores WHERE id = userId;
 END //
 DELIMITER ;
