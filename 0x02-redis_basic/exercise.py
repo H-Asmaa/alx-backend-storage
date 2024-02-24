063d2e571d046011718dd324c3991a7bc3193b8e
H-Asmaa <asmaehadar32@gmail.com>
2024-02-24 23:51:58 +0100

    Committing the class Cache with the main test.
    The class has the method store that generates a random uuid key and stores the input data in Redis using the random key and returns the key
    
    Fixing output fail.

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -15,10 +15,12 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[str, bytes, int]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """A method that generates a random uuid key and
         stores the input data in Redis using the random key
         and returns the key."""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+
