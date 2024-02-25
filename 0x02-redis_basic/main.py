3512632a65e7f9b4f05d8f3797baacaaa21d1821
H-Asmaa <asmaehadar32@gmail.com>
2024-02-25 03:06:53 +0100

    Committing new main test.

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -35,8 +35,6 @@ for value, fn in TEST_CASES.items():
 
 # TASK 2
 
-Cache = __import__('exercise').Cache
-
 cache2 = Cache()
 
 cache2.store(b"first")
@@ -45,3 +43,20 @@ print(cache2.get(cache2.store.__qualname__))
 cache2.store(b"second")
 cache2.store(b"third")
 print(cache2.get(cache2.store.__qualname__))
+
+# TASK 3
+
+cache3 = Cache()
+
+s1 = cache3.store("first")
+print(s1)
+s2 = cache3.store("secont")
+print(s2)
+s3 = cache3.store("third")
+print(s3)
+
+inputs = cache3._redis.lrange("{}:inputs".format(cache3.store.__qualname__), 0, -1)
+outputs = cache3._redis.lrange("{}:outputs".format(cache3.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
