3480c9bfcf072086192a73b28f818f828447bba4
H-Asmaa <asmaehadar32@gmail.com>
2024-02-26 20:28:15 +0100

    Committing the get_page function that gets the content of a page.
    The method uses a decorator to increment the count of the calls to a specific URL.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,16 +4,55 @@
 """
 import requests
 import redis
+from typing import Callable
+from functools import wraps
+from time import sleep
 
 
+redisInstance = redis.Redis()
+redisInstance.flushdb()
+
+
+def callsCount(method: Callable) -> Callable:
+    """A decorator that takes a method callable argument
+    that increments the callsCount."""
+
+    @wraps(method)
+    def incrCount(url: str) -> str:
+        """A method that increments the numbers of requests
+        done for a URL."""
+
+        countKey = f"count:{url}"
+        storageKey = f"storage:{url}"
+        storageVal = redisInstance.get(storageKey)
+        if storageVal:
+            redisInstance.incr(countKey)
+            return storageVal.decode("utf-8")
+        call = method(url)
+        redisInstance.setex(storageKey, 10, call)
+        redisInstance.set(countKey, 1)
+        return call
+
+    return incrCount
+
+
+@callsCount
 def get_page(url: str) -> str:
     """A method that uses the requests module to obtain the HTML
     content of a particular URL and returns it."""
-    redisInstance = redis.Redis()
-    callsCount = redisInstance.get(f"count:{url}")
-    callsCount = int(callsCount or 0) + 1
-    response = requests.get(url)
-    redisInstance.set(f"count:{url}", callsCount, ex=10)
+    try:
+        response = requests.get(url).text
+        return response
+    except requests.RequestException as e:
+        return
 
 
-get_page("http://slowwly.robertomurray.co.uk")
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk"
+    get_page(url)
+    get_page(url)
+    get_page(url)
+    get_page(url)
+    print(redisInstance.get(f"storage:{url}"))
+    sleep(12)
+    print(redisInstance.get(f"storage:{url}"))
